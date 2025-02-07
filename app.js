865eda0c7a48b2057c31fb4f19910721188f624b
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-02-07 10:09:59 +0200

    save

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-
 const app = express();
 
 // middleware
@@ -10,17 +10,12 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 // database connection
-
 const username = encodeURIComponent(process.env.DB_USERNAME);
 const password = encodeURIComponent(process.env.DB_PASSWORD);
 const dbURI = `mongodb+srv://${username}:${password}@nodeblog.wo7j5.mongodb.net/blogs?retryWrites=true&w=majority&appName=nodeblog`;
 
 mongoose
-  .connect(dbURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
+  .connect(dbURI)
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
